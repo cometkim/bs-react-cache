06188d9c644fa34759301d8eaaa7a5dd2edd87c7
Hyeseong Kim <cometkim.kr@gmail.com>
2020-04-27 05:19:54 +0900

    chore: Add generated js files

---
+++ examples/SuspenseImage.bs.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var ReactCache = require("react-cache");
+
+var imageResource = ReactCache.unstable_createResource((function (src) {
+        return new Promise((function (resolve, reject) {
+                      var image = new Image();
+                      image.onload = (function (param) {
+                          return resolve(src);
+                        });
+                      image.onerror = (function (exn) {
+                          return reject(exn);
+                        });
+                      image.src = src;
+                      return /* () */0;
+                    }));
+      }), undefined);
+
+imageResource.read("dot.png");
+
+imageResource.preload("cat.png");
+
+exports.imageResource = imageResource;
+/* imageResource Not a pure module */
